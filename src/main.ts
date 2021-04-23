accc562825355a5464b1f2e1f0cc66dcd8f04942
Brian Underwood <brian@brian-underwood.codes>
2021-04-23 21:25:13 +0200

    Require confirmation before leaving page

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,15 @@ import App from './App.vue'
 import 'bootstrap'
 
 createApp(App).mount('#app')
+
+window.onbeforeunload = function (e) {
+  e = e || window.event;
+
+  // For IE and Firefox prior to version 4
+  if (e) {
+    e.returnValue = 'Sure?';
+  }
+
+  // For Safari
+  return 'Sure?';
+};
