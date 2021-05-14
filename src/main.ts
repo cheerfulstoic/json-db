5a078fb698eb32bcf0c5ce07e48d84cd7938dcf0
Brian Underwood <brian@brian-underwood.codes>
2021-05-14 21:31:19 +0200

    Add rollbar

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
+import Rollbar from 'rollbar';
+
+Vue.prototype.$rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_POST_CLIENT_ITEM_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
 import 'bootstrap'
 
-createApp(App).mount('#app')
+let app = createApp(App)
+
+app.config.errorHandler = (err, vm, info) => {
+  vm.$rollbar.error(err);
+  throw err; // rethrow
+};
+
+app.mount('#app')
 
 window.onbeforeunload = function (e) {
   e = e || window.event;
